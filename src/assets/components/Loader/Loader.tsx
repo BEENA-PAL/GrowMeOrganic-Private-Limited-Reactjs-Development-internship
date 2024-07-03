6ab7e3cbd0955ba611c460b3f7293c1e962a552a
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-07-03 12:24:29 +0530

    first commit

---
+++ src/assets/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import "./Loader.css";
+const Loader = () => {
+  return (
+    <span className="loader">
+      <span className="loader_ball"></span>
+      <span className="loader_ball"></span>
+      <span className="loader_ball"></span>
+    </span>
+  );
+};
+
+export default Loader;
